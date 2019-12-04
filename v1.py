0c8042a620d01ed6ed5fa35b9a3eac147f1a2d48
yhlli <36483872+yhlli@users.noreply.github.com>
2019-12-03 21:01:26 -0500

    Add files via upload

--- v1.py
+++ v1.py
@@ -1,4 +1,3 @@
-!pip install meteocalc
 import pandas as pd
 import numpy as np
 import os
@@ -23,78 +22,85 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
-train_df = pd.read_csv('/kaggle/input/ashrae-energy-prediction/train.csv')
-building_df = pd.read_csv('/kaggle/input/ashrae-energy-prediction/building_metadata.csv')
-weather_df = pd.read_csv('/kaggle/input/ashrae-energy-prediction/weather_train.csv')
+train_df = pd.read_csv('train.csv')
+building_df = pd.read_csv('building_metadata.csv')
+weather_df = pd.read_csv('weather_train.csv')
+
 
 # Original code from https://www.kaggle.com/aitude/ashrae-missing-weather-data-handling by @aitude
 
 def fill_weather_dataset(weather_df):
-    
     # Find Missing Dates
     time_format = "%Y-%m-%d %H:%M:%S"
-    start_date = datetime.datetime.strptime(weather_df['timestamp'].min(),time_format)
-    end_date = datetime.datetime.strptime(weather_df['timestamp'].max(),time_format)
+    start_date = datetime.datetime.strptime(weather_df['timestamp'].min(), time_format)
+    end_date = datetime.datetime.strptime(weather_df['timestamp'].max(), time_format)
     total_hours = int(((end_date - start_date).total_seconds() + 3600) / 3600)
     hours_list = [(end_date - datetime.timedelta(hours=x)).strftime(time_format) for x in range(total_hours)]
 
     missing_hours = []
     for site_id in range(16):
         site_hours = np.array(weather_df[weather_df['site_id'] == site_id]['timestamp'])
-        new_rows = pd.DataFrame(np.setdiff1d(hours_list,site_hours),columns=['timestamp'])
+        new_rows = pd.DataFrame(np.setdiff1d(hours_list, site_hours), columns=['timestamp'])
         new_rows['site_id'] = site_id
-        weather_df = pd.concat([weather_df,new_rows])
+        weather_df = pd.concat([weather_df, new_rows])
 
-        weather_df = weather_df.reset_index(drop=True)           
+        weather_df = weather_df.reset_index(drop=True)
 
-    # Add new Features
+        # Add new Features
     weather_df["datetime"] = pd.to_datetime(weather_df["timestamp"])
     weather_df["day"] = weather_df["datetime"].dt.day
     weather_df["week"] = weather_df["datetime"].dt.week
     weather_df["month"] = weather_df["datetime"].dt.month
-    
+
     # Reset Index for Fast Update
-    weather_df = weather_df.set_index(['site_id','day','month'])
+    weather_df = weather_df.set_index(['site_id', 'day', 'month'])
 
-    air_temperature_filler = pd.DataFrame(weather_df.groupby(['site_id','day','month'])['air_temperature'].mean(),columns=["air_temperature"])
-    weather_df.update(air_temperature_filler,overwrite=False)
-
-    # Step 1
-    cloud_coverage_filler = weather_df.groupby(['site_id','day','month'])['cloud_coverage'].mean()
-    # Step 2
-    cloud_coverage_filler = pd.DataFrame(cloud_coverage_filler.fillna(method='ffill'),columns=["cloud_coverage"])
-
-    weather_df.update(cloud_coverage_filler,overwrite=False)
-
-    due_temperature_filler = pd.DataFrame(weather_df.groupby(['site_id','day','month'])['dew_temperature'].mean(),columns=["dew_temperature"])
-    weather_df.update(due_temperature_filler,overwrite=False)
+    air_temperature_filler = pd.DataFrame(weather_df.groupby(['site_id', 'day', 'month'])['air_temperature'].mean(),
+                                          columns=["air_temperature"])
+    weather_df.update(air_temperature_filler, overwrite=False)
 
     # Step 1
-    sea_level_filler = weather_df.groupby(['site_id','day','month'])['sea_level_pressure'].mean()
+    cloud_coverage_filler = weather_df.groupby(['site_id', 'day', 'month'])['cloud_coverage'].mean()
     # Step 2
-    sea_level_filler = pd.DataFrame(sea_level_filler.fillna(method='ffill'),columns=['sea_level_pressure'])
+    cloud_coverage_filler = pd.DataFrame(cloud_coverage_filler.fillna(method='ffill'), columns=["cloud_coverage"])
 
-    weather_df.update(sea_level_filler,overwrite=False)
+    weather_df.update(cloud_coverage_filler, overwrite=False)
 
-    wind_direction_filler =  pd.DataFrame(weather_df.groupby(['site_id','day','month'])['wind_direction'].mean(),columns=['wind_direction'])
-    weather_df.update(wind_direction_filler,overwrite=False)
-
-    wind_speed_filler =  pd.DataFrame(weather_df.groupby(['site_id','day','month'])['wind_speed'].mean(),columns=['wind_speed'])
-    weather_df.update(wind_speed_filler,overwrite=False)
+    due_temperature_filler = pd.DataFrame(weather_df.groupby(['site_id', 'day', 'month'])['dew_temperature'].mean(),
+                                          columns=["dew_temperature"])
+    weather_df.update(due_temperature_filler, overwrite=False)
 
     # Step 1
-    precip_depth_filler = weather_df.groupby(['site_id','day','month'])['precip_depth_1_hr'].mean()
+    sea_level_filler = weather_df.groupby(['site_id', 'day', 'month'])['sea_level_pressure'].mean()
     # Step 2
-    precip_depth_filler = pd.DataFrame(precip_depth_filler.fillna(method='ffill'),columns=['precip_depth_1_hr'])
+    sea_level_filler = pd.DataFrame(sea_level_filler.fillna(method='ffill'), columns=['sea_level_pressure'])
 
-    weather_df.update(precip_depth_filler,overwrite=False)
+    weather_df.update(sea_level_filler, overwrite=False)
+
+    wind_direction_filler = pd.DataFrame(weather_df.groupby(['site_id', 'day', 'month'])['wind_direction'].mean(),
+                                         columns=['wind_direction'])
+    weather_df.update(wind_direction_filler, overwrite=False)
+
+    wind_speed_filler = pd.DataFrame(weather_df.groupby(['site_id', 'day', 'month'])['wind_speed'].mean(),
+                                     columns=['wind_speed'])
+    weather_df.update(wind_speed_filler, overwrite=False)
+
+    # Step 1
+    precip_depth_filler = weather_df.groupby(['site_id', 'day', 'month'])['precip_depth_1_hr'].mean()
+    # Step 2
+    precip_depth_filler = pd.DataFrame(precip_depth_filler.fillna(method='ffill'), columns=['precip_depth_1_hr'])
+
+    weather_df.update(precip_depth_filler, overwrite=False)
 
     weather_df = weather_df.reset_index()
-    weather_df = weather_df.drop(['datetime','day','week','month'],axis=1)
-    
+    weather_df = weather_df.drop(['datetime', 'day', 'week', 'month'], axis=1)
+
     def get_meteorological_features(data):
         def calculate_rh(df):
-            df['relative_humidity'] = 100 * (np.exp((17.625 * df['dew_temperature']) / (243.04 + df['dew_temperature'])) / np.exp((17.625 * df['air_temperature'])/(243.04 + df['air_temperature'])))
+            df['relative_humidity'] = 100 * (
+                        np.exp((17.625 * df['dew_temperature']) / (243.04 + df['dew_temperature'])) / np.exp(
+                    (17.625 * df['air_temperature']) / (243.04 + df['air_temperature'])))
+
         def calculate_fl(df):
             flike_final = []
             flike = []
@@ -108,32 +114,35 @@ def fill_weather_dataset(weather_df):
                 flike_final.append(flike[i].f)
             df['feels_like'] = flike_final
             del flike_final, flike, at, rh, ws
+
         calculate_rh(data)
         calculate_fl(data)
         return data
 
     weather_df = get_meteorological_features(weather_df)
-    
+
     return weather_df
+
 
 # Original code from https://www.kaggle.com/gemartin/load-data-reduce-memory-usage by @gemartin
 
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
 from pandas.api.types import is_categorical_dtype
 
+
 def reduce_mem_usage(df, use_float16=False):
     """
-    Iterate through all the columns of a dataframe and modify the data type to reduce memory usage.        
+    Iterate through all the columns of a dataframe and modify the data type to reduce memory usage.
     """
-    
-    start_mem = df.memory_usage().sum() / 1024**2
+
+    start_mem = df.memory_usage().sum() / 1024 ** 2
     print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
-    
+
     for col in df.columns:
         if is_datetime(df[col]) or is_categorical_dtype(df[col]):
             continue
         col_type = df[col].dtype
-        
+
         if col_type != object:
             c_min = df[col].min()
             c_max = df[col].max()
@@ -145,7 +154,7 @@ def reduce_mem_usage(df, use_float16=False):
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
+                    df[col] = df[col].astype(np.int64)
             else:
                 if use_float16 and c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
@@ -156,59 +165,59 @@ def reduce_mem_usage(df, use_float16=False):
         else:
             df[col] = df[col].astype("category")
 
-    end_mem = df.memory_usage().sum() / 1024**2
+    end_mem = df.memory_usage().sum() / 1024 ** 2
     print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
     print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
-    
+
     return df
 
 
 def features_engineering(df):
-    
     # Sort by timestamp
     df.sort_values("timestamp")
     df.reset_index(drop=True)
-    
+
     # Add more features
-    df["timestamp"] = pd.to_datetime(df["timestamp"],format="%Y-%m-%d %H:%M:%S")
+    df["timestamp"] = pd.to_datetime(df["timestamp"], format="%Y-%m-%d %H:%M:%S")
     df["hour"] = df["timestamp"].dt.hour
     df["weekend"] = df["timestamp"].dt.weekday
     holidays = ["2016-01-01", "2016-01-18", "2016-02-15", "2016-05-30", "2016-07-04",
-                    "2016-09-05", "2016-10-10", "2016-11-11", "2016-11-24", "2016-12-26",
-                    "2017-01-02", "2017-01-16", "2017-02-20", "2017-05-29", "2017-07-04",
-                    "2017-09-04", "2017-10-09", "2017-11-10", "2017-11-23", "2017-12-25",
-                    "2018-01-01", "2018-01-15", "2018-02-19", "2018-05-28", "2018-07-04",
-                    "2018-09-03", "2018-10-08", "2018-11-12", "2018-11-22", "2018-12-25",
-                    "2019-01-01"]
+                "2016-09-05", "2016-10-10", "2016-11-11", "2016-11-24", "2016-12-26",
+                "2017-01-02", "2017-01-16", "2017-02-20", "2017-05-29", "2017-07-04",
+                "2017-09-04", "2017-10-09", "2017-11-10", "2017-11-23", "2017-12-25",
+                "2018-01-01", "2018-01-15", "2018-02-19", "2018-05-28", "2018-07-04",
+                "2018-09-03", "2018-10-08", "2018-11-12", "2018-11-22", "2018-12-25",
+                "2019-01-01"]
     df['group'] = df['timestamp'].dt.month
-    df['group'].replace((1, 2, 3, 4), 1, inplace = True)
-    df['group'].replace((5, 6, 7, 8), 2, inplace = True)
-    df['group'].replace((9, 10, 11, 12), 3, inplace = True)
+    df['group'].replace((1, 2, 3, 4), 1, inplace=True)
+    df['group'].replace((5, 6, 7, 8), 2, inplace=True)
+    df['group'].replace((9, 10, 11, 12), 3, inplace=True)
     df["is_holiday"] = (df.timestamp.isin(holidays)).astype(int)
-    df['square_feet'] =  np.log1p(df['square_feet'])
-    
-    
+    df['square_feet'] = np.log1p(df['square_feet'])
+
     # Remove Unused Columns
     drop = ["timestamp"]
     df = df.drop(drop, axis=1)
     gc.collect()
-    
+
     # Encode Categorical Data
     le = LabelEncoder()
     df["primary_use"] = le.fit_transform(df["primary_use"])
-    
+
     return df
+
+
 # weather manipulation
 weather_df = fill_weather_dataset(weather_df)
 
 # memory reduction
-train_df = reduce_mem_usage(train_df,use_float16=True)
-building_df = reduce_mem_usage(building_df,use_float16=True)
-weather_df = reduce_mem_usage(weather_df,use_float16=True)
+train_df = reduce_mem_usage(train_df, use_float16=True)
+building_df = reduce_mem_usage(building_df, use_float16=True)
+weather_df = reduce_mem_usage(weather_df, use_float16=True)
 
 # merge data
-train_df = train_df.merge(building_df, left_on='building_id',right_on='building_id',how='left')
-train_df = train_df.merge(weather_df,how='left',left_on=['site_id','timestamp'],right_on=['site_id','timestamp'])
+train_df = train_df.merge(building_df, left_on='building_id', right_on='building_id', how='left')
+train_df = train_df.merge(weather_df, how='left', left_on=['site_id', 'timestamp'], right_on=['site_id', 'timestamp'])
 del weather_df
 gc.collect()
 
@@ -217,3 +226,4 @@ train_df = features_engineering(train_df)
 
 # transform target variable
 train_df['meter_reading'] = np.log1p(train_df["meter_reading"])
+
