c7cbda75d3bfc1122d75170bf258adc43e690f2d
yhlli <36483872+yhlli@users.noreply.github.com>
2019-12-12 21:18:22 -0500

    Update dropBadLeaks.py

--- dropBadLeaks.py
+++ dropBadLeaks.py
@@ -1,6 +1,5 @@
-leak_df = pd.read_feather('kaggle/input/leak.feather')
-
-leak_df = leak_df [ leak_df['building_id'] != 13 ]
-leak_df = leak_df [ leak_df['building_id'] != 14 ]
-
+import pandas as pd
+leak_df = pd.read_feather('leak.feather')
+leak_df = leak_df[leak_df['building_id'] != 13].reset_index(drop=True)
+leak_df = leak_df[leak_df['building_id'] != 14].reset_index(drop=True)
 leak_df.to_feather('leak.feather')
